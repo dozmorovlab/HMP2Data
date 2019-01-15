154939e38920b0c22b0026eae5fcf56f183ea643
jstansfield0 <stansfieldjc@vcu.edu>
2019-01-15 16:49:31 -0500

    working on ibd, t2d data

---
+++ R/IBD16S.R
@@ -0,0 +1,6 @@
+#' HMP2 16S sequencing data for Irritable Bowel Disease (IBD)
+#'
+#' The HMP2 study gathered data on the microbiome for
+#' patients with different disease. This dataset contains
+#' 16S rRNA data for patients with IBD.
+#'
