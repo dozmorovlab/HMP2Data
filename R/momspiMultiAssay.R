24650522e9bd3c9fdc9e75bb8348843c6dcc61ee
jstansfield0 <stansfieldjc@vcu.edu>
2019-01-17 14:12:35 -0500

    work on multiassay

--- R/momspiMultiAssay.R
+++ R/momspiMultiAssay.R
@@ -1,12 +1,38 @@
 #' Make MOMS-PI MultiAssayExperiment
 #'
+#' Construct MultiAssayExperiment for MOMS-PI 16S rRNA
+#' and cytokines data.
 #'
-#'
+#' @format A MultiAssayExperiment object with a 16S rRNA matrix and Cytokine matrix
+#' \subsection{rRNA}{}
+#'     A counts matrix for the rRNA-seq results.
+#' }
+#' \subsection{cytokines} {
+#'     A counts matrix for the cytokines results.
+#' }
+#' \subsection{colData}{
+#'    \describe{
+#'       \item{sample_id}{Sample identifier}
+#'       \item{subject_id}{Participant identifier}
+#'       \item{sample_body_site}{Body site of the sample}
+#'       \item{visit_number}{Visit number}
+#'       \item{subject_gender}{Participant gender}
+#'       \item{subject_race}{Participant race}
+#'       \item{study_full_name}{Name of the study}
+#'       \item{file}{Filename which the sample was taken from}
+#'    }
+#' }
+#' @import MultiAssayExperiment
+#' @export
+#' @examples momspiMA <- momspiMultiAssay()
 momspiMultiAssay <- function() {
   # load MOMS-PI data
-  data("momspi16S")
-  data("momspiCytokines")
-  # pull out matrices
-  rna_mat <- otu_table(momspi16S) %>% as.matrix()
-  rna_samp <- sample_data(momspi16S)
+  data("momspi16S_mtx")
+  data("momspi16S_samp")
+  data("momspiCyto_mtx")
+  data("momspiCyto_samp")
+  # combine into multiAssayExperiment
+  momsMultiAssay <- MultiAssayExperiment(experiments = ExperimentList(rRNA = momspi16S_mtx, cytokines = momspiCyto_mtx),
+                                         colData = rbind(momspi16S_samp, momspiCyto_samp))
+  return(momsMultiAssay)
 }
